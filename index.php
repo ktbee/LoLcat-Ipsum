ffec2f61a11df2cf917ea5b28a02676d9ae3d065
Katie Broida <kbroida@gmail.com>
2015-08-30 21:26:43 -0400

    Added ability to choose just  latin or lolspeak

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<title>	LoLCat Ipsum</title>
 	<link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
 	<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
-	<link href="css/custom.css" rel="stylesheet">
+	<link href="custom.css" rel="stylesheet">
 	<?php require 'logic.php'; ?>
 			
 	
@@ -25,6 +25,12 @@
 				<label for='numPar'>How many paragraphs would you like? (max 15)</label>
 				<input type='number' name='numPar' id='numPar' max='15' min='1' required><br>
 				<br>
+				<label for='purrist'>Are you a purrist?</label>
+				<br>
+				<input type='radio' name='purrist' value='lolspeak'> Yes, I just want lolspeak
+				<br>
+				<input type='radio' name='purrist' value='latin'> Yes, I just want latin
+				<br>
 				<br>
 				<button type='submit' name='getLOL' class='btn btn-lg'>Click here to Laugh Out Latin</button>
 			</form>
@@ -34,7 +40,7 @@
 		<br>
 		
 					</span>
-		<img src="/grumpy-latin-cat.png">
+		<img src="grumpy-latin-cat.png" class="hidden-sm hidden-xs">
 
 		<?php 
 				if(isset($_GET['getLOL'])) {
